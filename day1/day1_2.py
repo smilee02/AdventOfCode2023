cb3f333385d7e9cba615f41d78fd023c313162bb
Francisco Cardoso <fffcardoso@gmail.com>
2023-12-21 21:19:03 +0000

    first commit

---
+++ day1/day1_2.py
@@ -0,0 +1,23 @@
+f = open('day1_2.txt')
+
+total = 0
+
+values_letters = {
+        "one": "o1e", 
+        "two": "t2o",
+        "three": "t3e",
+        "four": "f4r",
+        "five": "f5e",
+        "six": "s6x",
+        "seven": "s7n",
+        "eight": "e8t",
+        "nine": "n9e"}
+
+for line in f:
+    for word in values_letters.keys():
+        if word in line:
+            line = line.replace(word, values_letters[word])
+    num_list = list(filter(lambda x: x.isnumeric(),line))
+    value = ((num_list[0]) + (num_list[-1]))
+    total += int(value)
+print(total)
